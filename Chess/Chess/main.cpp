cf18dce056881c4379af9e0489b836e8a52c96d7
SurduTony <tony-stefanel.surdu@student.tuiasi.ro>
2023-04-05 01:51:11 +0300

    added game class

--- Chess/Chess/main.cpp
+++ Chess/Chess/main.cpp
@@ -1,19 +1,20 @@
 #include "SDL.h"
+#include "Game.h"
 #include <iostream>
 
 int main(int argc, char* argv[]) 
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	Game* game = new Game();
 
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	game->init("Chess", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
-	SDL_RenderClear(renderer);
+	while (game->running()) {
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
 
-	SDL_RenderPresent(renderer);
-
-	SDL_Delay(3000);
+	game->clean();
 
 	return 0;
 }
